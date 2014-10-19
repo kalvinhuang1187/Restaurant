6b2aa1f4cc91e25d8acb34079692e974a16f6a75
Kalvin Huang <kalvinhuang1187@gmail.com>
2014-10-19 14:54:56 -0700

    initial commit to Restaurant class and SpringConfig.xml; also injecting value into a bean property

---
+++ src/org/kalvinhuang/springcore/Restaurant.java
@@ -0,0 +1,19 @@
+package org.kalvinhuang.springcore;
+
+public class Restaurant {
+	
+	String welcomeNote;
+	
+	public String getWelcomeNote() {
+		return welcomeNote;
+	}
+
+	public void setWelcomeNote(String welcomeNote) {
+		this.welcomeNote = welcomeNote;
+	}
+
+	public void greetCustomer() {
+		System.out.println(welcomeNote);
+	}
+
+}
---
+++ src/org/kalvinhuang/springcore/TestSpringProject.java
@@ -0,0 +1,18 @@
+package org.kalvinhuang.springcore;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestSpringProject {
+	public static void main(String[] args) {
+		
+		//find/load SpringConfig.xml
+		ApplicationContext context =
+			new ClassPathXmlApplicationContext("SpringConfig.xml");
+		
+		//requesting Spring framework to create/return an object called restaurantBean  
+		Restaurant restaurantObj = (Restaurant) context.getBean ("restaurantBean");
+		
+		restaurantObj.greetCustomer();
+	}
+}
