3b9ba53177706370d9ed98baf5882eb3e9f584e1
Kalvin Huang <kalvinhuang1187@gmail.com>
2014-10-20 16:59:51 -0700

    bean life-cycle methods: init and destroy

--- src/org/kalvinhuang/springcore/Restaurant.java
+++ src/org/kalvinhuang/springcore/Restaurant.java
@@ -1,15 +1,24 @@
 package org.kalvinhuang.springcore;
 
 public class Restaurant {
-	
+	/*
 	public String welcomeNote;
 	
 	public void setWelcomeNotes(String welcomeNote) {
 		this.welcomeNote = welcomeNote;
 	}
+	*/
 	
 	public void greetCustomer() {
-		System.out.println(welcomeNote);
+		System.out.println("Welcome dear customer!! This is bean life cycle");
+	}
+	
+	public void init() {
+		System.out.println("Restaurant Bean is going through init.");
+	}
+	
+	public void destroy() {
+		System.out.println("Bean will destory now.");
 	}
 	
 }
--- src/org/kalvinhuang/springcore/TestSpringProject.java
+++ src/org/kalvinhuang/springcore/TestSpringProject.java
@@ -1,6 +1,7 @@
 package org.kalvinhuang.springcore;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class TestSpringProject {
@@ -10,13 +11,11 @@ public class TestSpringProject {
 		ApplicationContext context =
 			new ClassPathXmlApplicationContext("SpringConfig.xml");
 		
+		((AbstractApplicationContext)context).registerShutdownHook();
+		
 		//because scope="prototype", new restaurantBean instance is created each time
 		Restaurant restaurantObj1 = (Restaurant) context.getBean ("restaurantBean");
-		restaurantObj1.setWelcomeNotes("Object1 is setting welcome note property");
 		restaurantObj1.greetCustomer();
-		
-		Restaurant restaurantObj2 = (Restaurant) context.getBean ("restaurantBean");
-		restaurantObj2.greetCustomer();
 		
 	}
 }
