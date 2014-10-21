ef1b474dbfc70e8a9430f9007631bf7738ef21a7
Kalvin Huang <kalvinhuang1187@gmail.com>
2014-10-20 23:12:22 -0700

    bean life cycle using initializing bean and disposable bean interfaces

--- src/org/kalvinhuang/springcore/Restaurant.java
+++ src/org/kalvinhuang/springcore/Restaurant.java
@@ -1,22 +1,28 @@
 package org.kalvinhuang.springcore;
 
-import javax.annotation.PostConstruct;
-import javax.annotation.PreDestroy;
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
 
-public class Restaurant {
+public class Restaurant implements InitializingBean, DisposableBean{
 	
 	public void greetCustomer() {
 		System.out.println("Welcome dear customer!! This is bean life cycle");
 	}
-	
-	@PostConstruct
-	public void init() {
-		System.out.println("Restaurant Bean is going through init.");
+
+	// same thing as init-method and @PostConstruct annotation
+	// will call this immediately after initializing restaurantBean
+	@Override
+	public void afterPropertiesSet() throws Exception {
+		// TODO Auto-generated method stub
+		System.out.println("Restaurant Bean is going through afterPropertiesSet");
 	}
-	
-	@PreDestroy
-	public void destroy() {
-		System.out.println("Bean will destory now.");
+
+	// same thing as destroy-method and @PreDestroy annotation
+	// will call this just before restaurantBean's destruction
+	@Override
+	public void destroy() throws Exception {
+		// TODO Auto-generated method stub
+		System.out.println("Restaurant Bean is destroying now");
 	}
 	
 }
